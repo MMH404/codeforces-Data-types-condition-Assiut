1a1d1ceb0eb05493b722c9ded9f75a71590ae33f
Aetronyx <aethronyx.code@gmail.com>
2026-07-20 00:40:35 +0600

    first commit

---
+++ N.cpp
@@ -0,0 +1,11 @@
+#include<iostream>
+using namespace std;
+int main(){
+    char ch;
+    cin>> ch;
+    if(ch>='a' && ch<='z')
+    cout<<(char)(ch-32);
+    else
+    cout<<(char)(ch+32);
+    return 0;
+}
